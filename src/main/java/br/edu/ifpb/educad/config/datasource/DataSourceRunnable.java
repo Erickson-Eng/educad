1e5b52854de7d8c5ca91f4681b1b765d649b675a
Hercules <hercules.sousa@academico.ifpb.edu.br>
2022-09-19 23:22:37 -0300

    Populando endereços

--- src/main/java/br/edu/ifpb/educad/config/datasource/DataSourceRunnable.java
+++ src/main/java/br/edu/ifpb/educad/config/datasource/DataSourceRunnable.java
@@ -1,6 +1,8 @@
 package br.edu.ifpb.educad.config.datasource;
 
+import br.edu.ifpb.educad.entity.Address;
 import br.edu.ifpb.educad.entity.User;
+import br.edu.ifpb.educad.repository.AddressRepository;
 import br.edu.ifpb.educad.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,11 +18,18 @@ public class DataSourceRunnable implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         User user1 = new User("joao_silva", "123abc", "joao@gmail.com", "87991178225");
         User user2 = new User("marina_araujo", "321ljkh", "marina@gmail.com", "77991175525");
         userRepository.saveAll(Arrays.asList(user1, user2));
+
+        Address address1 = new Address("Rua dos Alfeneiros", "Casa", "40028922", "Remígio", "Paraíba", "40028922");
+        Address address2 = new Address("Rua das Pitangas", "Apto", "91051209", "Juazeiro", "Pernambuco", "40506090");
+        addressRepository.saveAll(Arrays.asList(address1, address2));
     }
 }
